e96134eb3cba277c92776d9007710ba0f8531ef5
ngduyd <nguyenduy2004.d@gmail.com>
2026-03-28 09:33:27 +0700

    feat: initialize application configuration module with environment variable support

--- app/core/config.py
+++ app/core/config.py
@@ -9,6 +9,7 @@ MQTT_USERNAME = os.environ.get("MQTT_USERNAME")
 MQTT_PASSWORD = os.environ.get("MQTT_PASSWORD")
 MQTT_TOPIC = os.environ.get("MQTT_TOPIC", "#")
 
+DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_PORT = int(os.environ.get("DB_PORT", 5432))
 DB_NAME = os.environ.get("DB_NAME", "sensors_db")
 DB_USER = os.environ.get("DB_USER", "user")
